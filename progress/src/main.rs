d16c029db2680a398461c1f159b060fc3378a8e4
Oliver Forral <intrepion@gmail.com>
2021-10-17 20:59:42 -0600

    basic example of a long-running for loop

--- progress/src/main.rs
+++ progress/src/main.rs
@@ -1,3 +1,12 @@
+use std::{time::Duration, thread::sleep};
+
+fn expensive_calculation(_n: &i32) {
+    sleep(Duration::from_secs(1));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let v = vec![1, 2, 3];
+    for n in v.iter() {
+        expensive_calculation(n);
+    }
 }
