0370353b56c05a4da072e2b19df7efae4e425a8b
Oliver Forral <intrepion@gmail.com>
2021-10-18 01:43:21 -0600

    show progress with bounds

--- progress/src/main.rs
+++ progress/src/main.rs
@@ -5,11 +5,20 @@ const CLEAR: &str = "\x1B[2J\x1B[1;1H";
 struct Progress<Iter> {
     iter: Iter,
     i: usize,
+    bound: Option<usize>,
 }
 
 impl<Iter> Progress<Iter> {
     pub fn new(iter: Iter) -> Self {
-        Progress { iter, i: 0 }
+        Progress { iter, i: 0, bound: None }
+    }
+}
+
+impl<Iter> Progress<Iter>
+where Iter: ExactSizeIterator {
+    pub fn with_bound(mut self) -> Self {
+        self.bound = Some(self.iter.len());
+        self
     }
 }
 
@@ -18,7 +27,15 @@ where Iter: Iterator {
     type Item = Iter::Item;
 
     fn next(&mut self) -> Option<Self::Item> {
-        println!("{}{}", CLEAR, "*".repeat(self.i));
+        print!("{}", CLEAR);
+        match self.bound {
+            Some(bound) =>
+                println!("[{}{}]",
+                    "*".repeat(self.i),
+                    " ".repeat(bound - self.i)),
+            None =>
+                println!("{}", "*".repeat(self.i))
+        };
         self.i += 1;
 
         self.iter.next()
@@ -41,7 +58,7 @@ fn expensive_calculation(_n: &i32) {
 
 fn main() {
     let v = vec![1, 2, 3];
-    for n in v.iter().progress() {
+    for n in v.iter().progress().with_bound() {
         expensive_calculation(n);
     }
 }
