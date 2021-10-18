0a088f21054604757fa6e5cec218f19033914dcc
Oliver Forral <intrepion@gmail.com>
2021-10-18 00:56:00 -0600

    refactor to progress function

--- progress/src/main.rs
+++ progress/src/main.rs
@@ -2,16 +2,20 @@ use std::{time::Duration, thread::sleep};
 
 const CLEAR: &str = "\x1B[2J\x1B[1;1H";
 
-fn expensive_calculation(_n: &i32) {
-    sleep(Duration::from_secs(1));
-}
-
-fn main() {
-    let v = vec![1, 2, 3];
+fn progress(v: Vec<i32>) {
     let mut i = 1;
     for n in v.iter() {
         println!("{}{}", CLEAR, "*".repeat(i));
         i += 1;
         expensive_calculation(n);
     }
+}
+
+fn expensive_calculation(_n: &i32) {
+    sleep(Duration::from_secs(1));
+}
+
+fn main() {
+    let v = vec![1, 2, 3];
+    progress(v);
 }
