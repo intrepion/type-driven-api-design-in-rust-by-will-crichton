8315477766e80a0000cf4fc727cf17c70b956f92
Oliver Forral <intrepion@gmail.com>
2021-10-18 01:07:11 -0600

    made progress generic over all things iterable

--- progress/src/main.rs
+++ progress/src/main.rs
@@ -2,12 +2,14 @@ use std::{time::Duration, thread::sleep};
 
 const CLEAR: &str = "\x1B[2J\x1B[1;1H";
 
-fn progress<T>(v: Vec<T>, f: fn(&T) -> ()) {
+fn progress<T, Iter>(iter: Iter, f: fn(T) -> ())
+where Iter: Iterator<Item = T> {
+
     let mut i = 1;
-    for n in v.iter() {
+    for n in iter {
         println!("{}{}", CLEAR, "*".repeat(i));
         i += 1;
-        f(n);
+        f (n);
     }
 }
 
@@ -17,5 +19,10 @@ fn expensive_calculation(_n: &i32) {
 
 fn main() {
     let v = vec![1, 2, 3];
-    progress(v, expensive_calculation);
+    progress(v.iter(), expensive_calculation);
+
+    use std::collections::HashSet;
+    let mut h = HashSet::new();
+    h.insert(0);
+    progress(h.iter(), expensive_calculation);
 }
